45297c3eff36c05e8bfe46df0b5066f4ed8abf1a
Dani-Bcn <nneodani@gmail.com>
2023-08-02 17:58:01 +0200

    responsive design

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,26 @@
 import "./App.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import MainHome from "./MainHome";
 import Responsive from "./Responsive";
 
 function App() {
-
   const [activeResponsive, setActiveResponsive] = useState();
+  useEffect(() => {
+    screen.orientation.type === "portrait-primary"
+      ? setActiveResponsive(true)
+      : setActiveResponsive(false);
+  }, []);
 
   const responsive = (e) => {
     e === "portrait-primary"
-      ? setActiveResponsive(true) : setActiveResponsive(false);      
+      ? setActiveResponsive(true)
+      : setActiveResponsive(false);
   };
 
   return (
     <main>
-      <MainHome activeResponsive={activeResponsive}/>
-      <Responsive responsive={responsive}/>
+      <MainHome activeResponsive={activeResponsive} />
+      <Responsive responsive={responsive} />
     </main>
   );
 }
