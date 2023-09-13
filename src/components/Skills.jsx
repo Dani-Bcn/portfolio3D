1b64c32c7f2bc15ee86d0f2ddf4ae5582f2fdc57
Dani-Bcn <nneodani@gmail.com>
2023-09-13 09:43:25 +0200

    Test

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -4,30 +4,18 @@ import ScrollTrigger from "gsap/ScrollTrigger";
 import { Center } from "@react-three/drei";
 export default function Skills() {
   gsap.registerPlugin(ScrollTrigger);
-  const wordsFrontEnd = ["Html", "Css", "Js", "React","Api-rest","npm"];
-  const wordsBackEnd = ["Node.js", "Express", "MongoDB", "Postman"];
-  const wordsDesign = ["Taldwin","Gsap","Framer-motion","Three.js","Photoshop" ];
+  const wordsFrontEnd = ["Html", "Css", "Js", "React","Api-rest","npm","Node.js", "Express", "MongoDB", "Postman","Taldwin","Gsap","Framer-motion","Three.js","Photoshop" ];
+
   const tl = gsap.timeline();
 
   useEffect(() => {
     var skillsFront = document.querySelectorAll("#skillsFront");
-    var skillsBack = document.querySelectorAll("#skillsBack");
-    var skillsDesign = document.querySelectorAll("#skillsDesign");
+   
     tl.set(skillsFront, {  
       opacity:0,  
       color: "rgb(50,250,250)",
       delay:1
-    });
-    tl.set(skillsBack, {  
-      opacity:0,  
-      color: "rgb(50,250,250)",
-      delay:1
-    });
-    tl.set(skillsDesign, {  
-      opacity:0,  
-      color: "rgb(50,250,250)",
-      delay:1
-    });
+    });   
 
     for (let x = 0; x < wordsFrontEnd.length; x++) {
       console.log(skillsFront[x].offsetTop)
@@ -44,55 +32,23 @@ export default function Skills() {
         delay:0.5
       });
     }
-
-  for (let x = 0; x < wordsBackEnd.length; x++) {
-    console.log(skillsBack[x].offsetTop)
-    tl.to(skillsBack[x], {
-      scrollTrigger: {
-        trigger: skillsBack[x],
-        start: "center, center",
-        end:100,
-        scrub: 1,
-      },
-      opacity:1,
-      width:100,
-      height:100,
-    });
-  }
-  for (let x = 0; x < wordsDesign.length; x++) {
-    console.log(skillsDesign[x].offsetTop)
-    tl.to(skillsDesign[x], {
-      scrollTrigger: {
-        trigger: skillsDesign[x],
-        start: "center, center",
-        end:100,
-        scrub: 1,
-      },
-      opacity:1,
-      width:100,
-      height:100,
-    });
-  }
-
-
 });
-
 
   return (
     <section
       id="skills"
       className="
-    flex
-    flex-col
-    items-center
-    justify-between
-    w-[80%]
-    mx-10
-    h-[1000px]
-    bg-gradient-to-t to-orange-200 from-slate-900/[0]
-    rounded-tl-[150px]
-    rounded-tr-[150px]
-    z-10
+      flex
+      flex-col
+      items-center
+      justify-between
+      w-[80%]
+      m-auto
+      h-[1000px]
+      bg-gradient-to-t to-orange-200 from-slate-900/[0]
+      rounded-tl-[150px]
+      rounded-tr-[150px]
+      z-10
   "
     >
       <h2
@@ -104,26 +60,15 @@ export default function Skills() {
       >
         Skills
       </h2>
-      <article
-        className="
-      flex
-      items-center
-      justify-between
-      w-[110%]
-      h-screen  
-      mt-52
-      "
-      >
+      
         <section
           className="
-          flex
-          flex-col 
-          items-center         
-          h-[1200px]               
+         float-right
+          w-[390px]
+          h-[1200px]     
+          bg-slate-600          
         "
-        >
-          {" "}
-          <h4> F-end </h4>
+        >                
           {
           wordsFrontEnd.map((e, i) => {
            console.log(i)
@@ -132,12 +77,9 @@ export default function Skills() {
                 key={i}
                 id="skillsFront"
                 className="
-                  flex                              
-                  items-center
-                  justify-center
-                  w-0
-                  h-0
-                  my-2
+                float-left   
+                my-5
+                mx-[14px]
                   bg-slate-300
                   clip-custom
                   opacity:0
@@ -148,71 +90,6 @@ export default function Skills() {
             );
           })}
         </section>
-        <section
-          className="
-          flex
-          flex-col
-          items-center
-          h-[1200px]       
-                      
-        "
-        >
-          <h4>B-end </h4>
-          {wordsBackEnd.map((e, i) => {
-            return (
-              <p
-              key={i}
-              id="skillsBack"
-                className="
-                flex                              
-                items-center
-                justify-center
-                w-0
-                h-0
-                my-2
-                bg-slate-300
-                clip-custom
-                opacity:0
-                "
-              >
-                {e}
-              </p>
-            );
-          })}
-        </section>
-        <section
-          className="
-          flex
-          flex-col
-          items-center  
-          h-[1200px]       
-        "
-        >
-          <h4> Design</h4>
-          {wordsDesign.map((e, i) => {
-            return (
-              <p
-                id="skillsDesign"
-                className="
-                flex                              
-                items-center
-                justify-center
-                w-0
-                h-0
-                my-2
-                bg-slate-300
-                clip-custom
-                opacity:0
-                text-1xl
-                "
-                key={i}
-              >
-                {e}
-              </p>
-            );
-          })}
-        </section>
-      </article>
     </section>
   );
 }
