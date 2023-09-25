8398559ad891de6f6c4527ed24012090e2f851d3
Dani-Bcn <nneodani@gmail.com>
2023-09-25 16:20:33 +0200

    Test links webs

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -11,6 +11,7 @@ export default function Projects() {
       img: images[0],
       margin: "-ml-16",
       link: "https://pokeahorcado.netlify.app/",
+      display: "hidden",
     },
     {
       id: "movies",
@@ -18,12 +19,14 @@ export default function Projects() {
       img: images[1],
       margin: "ml-16",
       link: "https://apiappmovies.netlify.app/",
+      display: "hidden",
     },
     {
       id: "home",
       text: "Homeworks for kids",
       img: images[2],
       margin: "-ml-20",
+      display: "block",
     },
   ];
 
@@ -53,13 +56,24 @@ export default function Projects() {
       backgroundColor: "white",
     });
 
+    gsap.to("#ct-links", {
+      scrollTrigger: {
+        trigger: "#ct-links",
+        start: "center -300",
+        end: 1000,
+        scrub: 2,
+      },
+      marginLeft: -75,
+      delay: 1,
+    });
+
     const tl = gsap.timeline();
 
     for (let x = 0; x < arrayProjects.length; x++) {
       tl.to(idProjects[x], {
         scrollTrigger: {
           trigger: idProjects[x],
-          start: "center 500",
+          start: "center 700",
           end: 1000,
           scrub: 2,
         },
@@ -97,12 +111,12 @@ export default function Projects() {
         <h2
           id="textProjects"
           className="    
-        opacity-0               
-        mt-32
-        ml-[-200px]
-        h-24
-        text-slate-50
-      "
+          opacity-0               
+          mt-32
+          ml-[-200px]
+          h-24
+          text-slate-50
+        "
         >
           Projects
         </h2>
@@ -119,38 +133,93 @@ export default function Projects() {
         ></div>
         <section
           className="
-        w-[800px]
-        h-[1500px]
-        my-10
-        flex
-        flex-col
-        items-center   
-        justify-between        
-      "
+          w-[800px]
+          h-[1500px]
+          my-10
+          flex
+          flex-col
+          items-center   
+          justify-between        
+        "
         >
           {arrayProjects.map((e, i) => {
             return (
               <article
                 className="
-                flex
-                flex-col
-                items-center
-                justify-between              
-              "
+                    flex
+                    flex-col
+                    items-center
+                    justify-between              
+                  "
                 key={i}
               >
                 <h3 className="text-center">{e.text}</h3>,
-                <section className="h-68">
+                <section
+                  className="
+                  flex
+                  items-center
+                 "
+                >
                   <a href={e.link} target="blank">
-                    <img
-                      id="idProjects"
-                      className={`
+                    <div
+                      className={`                     
+                        w-screen
+                        1/2
+                        flex       
+                        items-center  
+                      `}
+                    >
+                      <img
+                        id="idProjects"
+                        className={`
                         h-96
                         clip-circle-0
                         ${e.margin}
                       `}
-                      src={e.img}
-                    />
+                        src={e.img}
+                      />
+                      <div
+                        id="ct-links"
+                        className="
+                          ml-32
+                          bg-slate-550
+                        "
+                      >
+                        {" "}
+                        <a href="https://github.com/Dani-Bcn/Home-works-frontEnd" target="blank">
+                          <div
+                            className={`
+                          w-36                        
+                          h-36
+                          flex
+                          items-center
+                          justify-center
+                          bg-red-300       
+                          clip-circle-50 
+                          ${e.display}
+                        `}
+                          >
+                            Front-end
+                          </div>
+                        </a>
+                        <a href="https://github.com/Dani-Bcn/Home-works-backEnd" target="blank">
+                          <div
+                            className={`
+                          w-36
+                          h-36
+                          flex
+                          items-center
+                          justify-center
+                        bg-red-300       
+                          clip-circle-50   
+                          ${e.display}
+                        `}
+                          >
+                            Back-end
+                          </div>
+                        </a>
+                      </div>
+                    </div>
                   </a>
                 </section>
               </article>
